760e876e51da2111b2f1f555312a835c3c62d85f
kgncsr <69785776+kgncsr@users.noreply.github.com>
2021-01-17 17:43:36 +0300

    Add project files.

---
+++ ClassMethodDemo/MusteriMenager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriMenager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müsteri eklendi : "+customer.Name);
+        }
+
+        public void List(Customer[] customers)
+        {
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item.SurName);
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Musteri Silindi : "+customer.Id);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yazılım Geliştirici Yetiştirme Kampı 3.gün ödevi
+            //Bir bankada müşteri takibi yapmak istiyorsunuz.
+            //Musteri isimli bir Class oluşturunuz.Müşteriye istediğiniz özellikleri ekleyiniz. (Id, Ad, Soyad....)
+            //MusteriManager sınıfı oluşturunuz.Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+
+            Customer customer1 = new Customer();
+            customer1.Id = 1001;
+            customer1.Name = "Ali";
+            customer1.SurName = "Yilmaz";
+            customer1.Country = "Turkiye";
+            customer1.City = "İstanbul";
+
+            Customer customer2 = new Customer() { Id = 1002, Name = "Alara", SurName = "Kenny", Country = "USA", City = "New York" };
+
+            Customer[] customers = new Customer[]
+            {
+                customer1,customer2
+            };
+
+
+            MusteriMenager musteriMenager = new MusteriMenager();
+            musteriMenager.Add(customer1);
+            musteriMenager.Add(customer2);
+            musteriMenager.List(customers);
+            musteriMenager.Delete(customer1);
+
+        }
+    }
+}
